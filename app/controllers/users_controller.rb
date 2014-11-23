3b414de57c04bbd97ad88c75935045f226a11c6e
Jesper Bränn <brann.jesper@gmail.com>
2014-11-23 12:08:31 +0100

    Add user check if they exist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ActionController::Base
+  def index
+    @user = User.find_by(email: params[:email])
+    puts params.inspect
+    if @user.nil?
+      render status: 404
+    else
+      render status: 200
+    end
+  end
+
+  def create
+    @user = User.new(params[:user])
+
+    if @user.save
+      render :json, true.to_json
+    else
+      render :json, false.to_json
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'landing_pages#index'
+
+  resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -11,20 +12,6 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
 
   # Example resource route with sub-resources:
   #   resources :products do
