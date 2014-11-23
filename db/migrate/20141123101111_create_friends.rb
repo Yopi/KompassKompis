76cff0f77eb8e56855cc4ebab5da2ccce9533510
Jesper Bränn <brann.jesper@gmail.com>
2014-11-23 16:56:47 +0100

    tjena

--- db/migrate/20141123101111_create_friends.rb
+++ db/migrate/20141123101111_create_friends.rb
@@ -1,6 +1,7 @@
 class CreateFriends < ActiveRecord::Migration
   def change
-    create_table :friends do |t|
+    create_table :friends, id: false do |t|
+      t.integer :id
       t.integer :friend_id
       t.timestamps
     end
